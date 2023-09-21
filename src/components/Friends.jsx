4856c6c0845a3883d437750d695f157aed2f03dc
teya-teya <teya.teya89@yandex.ru>
2023-09-21 10:55:27 +0300

    add assets/img

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,11 +1,15 @@
 import { NavLink } from "react-router-dom";
+import pageImg from "../asset/img/sea.jpg";
 
 const TableRow = (props) => {
   return (
     <tr>
       <th scope="row">{props.index + 1}</th>
       <td>
-        <NavLink to = {"/profile/" + props.id}>{props.name} {props.lastname}</NavLink></td>
+        <NavLink to={"/profile/" + props.id}>
+          {props.name} {props.lastname}
+        </NavLink>
+      </td>
       <td>{props.email}</td>
     </tr>
   );
@@ -18,23 +22,35 @@ export const Friends = (props) => {
 
   for (let i = 0; i < userCount; i++) {
     userRow.push(
-      <TableRow index={i} key={i} id={users[i].id} name={users[i].name} lastname={users[i].lastname} email={users[i].email} />
+      <TableRow
+        index={i}
+        key={i}
+        id={users[i].id}
+        name={users[i].name}
+        lastname={users[i].lastname}
+        email={users[i].email}
+      />
     );
   }
   return (
-    <div className="row">
+    <div className="container-fluid">
       <h1 className="text-center">Друзья</h1>
-      <div className="col-12">
-        <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Имя и Фамилия</th>
-              <th scope="col">Email</th>
-            </tr>
-          </thead>
-          <tbody>{userRow}</tbody>
-        </table>
+      <div className="row">
+        <div className="col-12">
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Имя и Фамилия</th>
+                <th scope="col">Email</th>
+              </tr>
+            </thead>
+            <tbody>{userRow}</tbody>
+          </table>
+        </div>
+        <div className="col-12 mt-5">
+          <img src={pageImg} alt="" width="100%" />
+        </div>
       </div>
     </div>
   );
